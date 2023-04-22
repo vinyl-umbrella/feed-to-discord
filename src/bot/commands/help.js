ce8964b4bb6c17cdeb524ca2472ea179c5346a07
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2023-04-22 12:14:02 +0900

    add help command

---
+++ src/bot/commands/help.js
@@ -0,0 +1,11 @@
+// const { EmbedBuilder, PermissionsBitField } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('help about F2D'),
+  async execute(interaction) {
+    await interaction.reply('/list: このチャンネルの登録済みのRSSを表示する\n/listall: このサーバの登録済みのRSSを表示する\n/register: RSSのURLを登録する\n/unsubscribe: RSSのURLを登録解除する\n/help: ヘルプを表示する');
+  },
+};
