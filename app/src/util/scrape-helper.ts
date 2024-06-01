ef24ba78140fa69d77e2eeb5c4204d91ccf1acd9
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2024-06-01 10:55:01 +0900

    add: function comments

--- app/src/util/scrape-helper.ts
+++ app/src/util/scrape-helper.ts
@@ -6,15 +6,35 @@ const RequestHeaders = {
     'ext/html,application/xhtml+xml,application/xml,application/rss+xml,application/atom+xml;q=0.9,*/*;q=0.8',
 };
 
+const FeedParser = new Parser();
+
+/**
+ * sleep for the given seconds
+ * @param {number} seconds
+ * @returns {Promise<void>}
+ */
 async function sleep(seconds: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 }
 
+/**
+ * fetch the feed from the given url using `fetch()`
+ * @param {string} url
+ * @returns {Promise<any>}
+ */
 async function fetchFeed(url: string): Promise<any> {
   const res = await fetch(url, {
     headers: RequestHeaders,
   });
 
+  return await res.text();
+}
+
+/**
+ * scrape the given url and return the feed object
+ * @param {string} url
+ * @returns {Promise<any>}
+ */
 async function scrapeFeed(url: string): Promise<any> {
   const body = await fetchFeed(url);
   const feed = await FeedParser.parseString(body);
@@ -22,6 +42,11 @@ async function scrapeFeed(url: string): Promise<any> {
   return feed;
 }
 
+/**
+ * check if the given url is a valid RSS feed
+ * @param {string} url
+ * @returns {boolean}
+ */
 async function canParseRSS(url: string): Promise<boolean> {
   try {
     await scrapeFeed(url);
